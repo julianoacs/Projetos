99ec2640131d7a170bb60b06ce0e049af9861619
Juliano Silva <123031321+julianoacs@users.noreply.github.com>
2023-09-16 22:51:23 -0300

    JoKenPo
    Adicionado jogo de JoKenPo feito Java.

---
+++ JoKenPo/JoKenPo_em_Java/src/App.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+class App {
+    public static void main(String[] args) throws Exception {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("----JoKenPo----");
+    System.out.println("1 - Pedra");
+    System.out.println("2 - Papel");
+    System.out.println("3 - Tesoura");
+    System.out.print("Digite sua opção: ");
+    int player = keyboard.nextInt();
+    switch (player) {
+        case 1:
+            System.out.println("Você escolheu Pedra");
+            break;
+        case 2:
+            System.out.println("Você escolheu Papel");
+            break;
+        case 3:
+            System.out.println("Você escolheu Tesoura");
+            break;
+        default:
+            System.out.println("Opção inválida");
+            break;
+    }
+    int computer = 0;
+    computer = (int) (Math.random() * 3 + 1);
+    switch (computer) {
+        case 1:
+            System.out.println("Computador escolheu Pedra");
+            break;
+        case 2:
+            System.out.println("Computador escolheu Papel");
+            break;
+        case 3:
+            System.out.println("Computador escolheu Tesoura");
+            break;
+    }
+    if (player == computer) {
+        System.out.println("Empate");
+    } else if ((player == 1 && computer == 3) || (player == 2 && computer == 1)
+            || (player == 3 && computer == 2)) {
+        System.out.println("Você Ganhou");
+    } else {
+        System.out.println("Computador Ganhou");
+    }
+    }
+}
--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 
 * Repositorio GitHub para iniciantes. [Clique Aqui](https://github.com/julianoacs/100_Exercises)
   * Repositório contem uma lista com mais de 80 exercícios, para treinar sua lógica de programação em Linguagem **Java**.
+* JoKenPo
+  * Jogo JoKenPo (Pedra, Papel ou Tesoura)
   
 <hr>
